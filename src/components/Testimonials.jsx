e21bbf0f8f3c98f2f918a44ed1f99bfe5b2b0eaf
GaziRiad <ouhibimustapha36@gmail.com>
2023-10-28 20:16:04 +0100

    Fixed path in testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,6 +1,10 @@
 import Heading from "./Heading";
+import person1 from "../assets/testimonials/person1.png";
+import person2 from "../assets/testimonials/person2.png";
+import person3 from "../assets/testimonials/person3.png";
+import person4 from "../assets/testimonials/person4.png";
+import person5 from "../assets/testimonials/person5.png";
 
-import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -11,6 +15,7 @@ import "swiper/css/pagination";
 // import required modules
 import { Mousewheel, Pagination } from "swiper/modules";
 import { FaQuoteLeft } from "react-icons/fa6";
+import { testimonials } from "../constants";
 
 function Testimonials() {
   return (
@@ -18,26 +23,11 @@ function Testimonials() {
       <Heading>Testimonials</Heading>
       <div className="flex flex-col items-center justify-left gap-8 px-4 lg:px-4 lg:gap-40 lg:flex-row  ">
         <div className=" flex flex-wrap items-center w-full justify-center xl:w-2/4 ">
-          <img
-            src="../assets/testimonials/person2.png"
-            className="lg:order-4"
-          />
-          <img
-            src="../assets/testimonials/person1.png"
-            className=" lg:order-3"
-          />
-          <img
-            src="../assets/testimonials/person3.png"
-            className="lg:order-2"
-          />
-          <img
-            src="../assets/testimonials/person4.png"
-            className="lg:order-1"
-          />
-          <img
-            src="../assets/testimonials/person5.png"
-            className="lg:order-0"
-          />
+          <img src={person1} className="lg:order-4" />
+          <img src={person2} className=" lg:order-3" />
+          <img src={person3} className="lg:order-2" />
+          <img src={person4} className="lg:order-1" />
+          <img src={person5} className="lg:order-0" />
         </div>
         <div className="w-full">
           <Swiper
@@ -52,82 +42,23 @@ function Testimonials() {
             modules={[Mousewheel, Pagination]}
             className="mySwiperVertical h-80 "
           >
-            <SwiperSlide className="ml-8 flex items-center gap-6 lg:ml-12 2xl:ml-32 ">
-              <span>
-                <FaQuoteLeft
-                  size={48}
-                  className=" scale-75 md:scale-100"
-                  color="#ffb800"
-                />
-              </span>
-              <p className="w-2/3 md:text-lg md:leading-8">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </SwiperSlide>
-
-            <SwiperSlide className="ml-8 flex items-center gap-6 lg:ml-12 2xl:ml-32">
-              <span>
-                <FaQuoteLeft
-                  size={48}
-                  className=" scale-75 md:scale-100"
-                  color="#ffb800"
-                />
-              </span>
-              <p className="w-2/3 md:text-lg md:leading-8">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </SwiperSlide>
-            <SwiperSlide className="ml-8 flex items-center gap-6 lg:ml-12 2xl:ml-32">
-              <span>
-                <FaQuoteLeft
-                  size={48}
-                  className=" scale-75 md:scale-100"
-                  color="#ffb800"
-                />
-              </span>
-              <p className="w-2/3 md:text-lg md:leading-8">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </SwiperSlide>
-            <SwiperSlide className="ml-8 flex items-center gap-6 lg:ml-12 2xl:ml-32">
-              <span>
-                <FaQuoteLeft
-                  size={48}
-                  className=" scale-75 md:scale-100"
-                  color="#ffb800"
-                />
-              </span>
-              <p className="w-2/3 md:text-lg md:leading-8">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </SwiperSlide>
-            <SwiperSlide className="ml-8 flex items-center gap-6 lg:ml-12 2xl:ml-32">
-              <span>
-                <FaQuoteLeft
-                  size={48}
-                  className=" scale-75 md:scale-100"
-                  color="#ffb800"
-                />
-              </span>
-              <p className="w-2/3 md:text-lg md:leading-8">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </SwiperSlide>
+            {testimonials.map((testimonial) => (
+              <SwiperSlide
+                key={testimonial.id}
+                className="ml-8 flex items-center gap-6 lg:ml-12 2xl:ml-32 "
+              >
+                <span>
+                  <FaQuoteLeft
+                    size={48}
+                    className=" scale-75 md:scale-100"
+                    color="#ffb800"
+                  />
+                </span>
+                <p className="w-2/3 md:text-lg md:leading-8">
+                  {testimonial.text}
+                </p>
+              </SwiperSlide>
+            ))}
           </Swiper>
         </div>
       </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -154,3 +154,22 @@ export const platformDetails = {
     },
   ],
 };
+
+export const testimonials = [
+  {
+    id: 1,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: 2,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: 3,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: 4,
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+];
