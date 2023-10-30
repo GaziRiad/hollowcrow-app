418343d305506162d859a98c6358e5456196d60d
GaziRiad <ouhibimustapha36@gmail.com>
2023-10-30 22:49:51 +0100

    Fixed cards section

--- src/components/CardsSection.jsx
+++ src/components/CardsSection.jsx
@@ -1,33 +1,34 @@
 import useContentHowItWorks from "../hooks/useContentHowItWorks";
-import Heading from "./Heading";
 
 function CardsSection() {
   const content = useContentHowItWorks();
 
   return (
-    <section className="container mx-auto grid grid-cols-1 justify-items-center gap-y-8 md:grid-cols-2 lg:gap-y-12 lg:gap-x-10 lg:px-24 xl:px-0 xl:grid-cols-4 xl:gap-x-16  mb-20 lg:mb-44  ">
-      {content.cardsSection.map((card) => (
-        <div
-          key={card.title}
-          className="shadow-md w-[70%] px-8 py-6 flex flex-col items-center gap-4 rounded-xl md:w-[95%] lg:w-full"
-        >
-          <img src={card.icon} className=" w-20" />
-          <Heading type="h4" style="text-[#ffb800]">
-            {card.title}
-          </Heading>
-          <ul className="flex flex-wrap gap-4">
-            {card.options.map((text) => (
-              <li
-                key={text}
-                className="text-base text-black-400 text-center leading-tight shadow-md bg-white rounded-full px-3 py-2"
-              >
-                {text}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </section>
+    <div className="bg-grayish-200 py-32">
+      <section className=" container mx-auto grid grid-cols-1 justify-items-center gap-y-8 md:grid-cols-2 lg:gap-y-12 lg:gap-x-10 lg:px-24 xl:px-0 xl:grid-cols-4 xl:gap-x-16 mb-12 lg:mb-20  ">
+        {content.cardsSection.map((card) => (
+          <div
+            key={card.title}
+            className="bg-white shadow-md w-[95%] px-8 py-6 flex flex-col items-center gap-4 rounded-xl sm:w-[70%] md:w-[95%] lg:w-full"
+          >
+            <img src={card.icon} className=" w-20" />
+            <p className="text-xl mb-4 text-center lg:text-left lg:text-2xl text-primary font-semibold">
+              {card.title}
+            </p>
+            <ul className="flex flex-wrap gap-4">
+              {card.options.map((text) => (
+                <li
+                  key={text}
+                  className="text-base text-black-400 text-center leading-tight shadow-md bg-white rounded-full px-3 py-2"
+                >
+                  {text}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </section>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ export default {
         "black-800": "#161616",
         "black-700": "#1E1E1E",
         "black-400": "#A0A6B5",
+        "grayish-200": "#FAFAFA",
       },
       screens: {
         "3xl": "1650px",
