15014ab19c0cfd549f33fe4b657838ec9efc42c5
GaziRiad <ouhibimustapha36@gmail.com>
2023-10-31 01:42:33 +0100

    Fixed padding in cardsSection

--- src/components/CardsSection.jsx
+++ src/components/CardsSection.jsx
@@ -5,11 +5,11 @@ function CardsSection() {
 
   return (
     <div className="bg-grayish-200 py-4 lg:py-32">
-      <section className=" container mx-auto grid grid-cols-1 justify-items-center gap-y-8 md:grid-cols-2 lg:gap-y-12 lg:gap-x-10 lg:px-24 xl:px-0 xl:grid-cols-4 xl:gap-x-16 mb-12 lg:mb-20  ">
+      <section className=" container mx-auto grid grid-cols-1 justify-items-center gap-y-8 md:grid-cols-2 lg:gap-y-12 lg:gap-x-10 lg:px-24 xl:px-0 xl:grid-cols-4 xl:gap-4 2xl:gap-x-16 mb-12 lg:mb-20  ">
         {content.cardsSection.map((card) => (
           <div
             key={card.title}
-            className="bg-white shadow-md w-[90%] px-4 py-6 flex flex-col items-center gap-4 rounded-xl sm:px-8 sm:w-[70%] md:w-[95%] lg:w-full"
+            className="bg-white shadow-md w-[90%] px-4 py-6 flex flex-col items-center gap-4 rounded-xl sm:px-8 sm:w-[70%] md:w-[95%] lg:w-full xl:px-2 2xl:px-8"
           >
             <img src={card.icon} className=" w-20" />
             <p className="text-xl mb-4 text-center lg:text-left lg:text-2xl text-primary font-semibold">
