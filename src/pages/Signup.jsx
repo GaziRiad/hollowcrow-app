5fce82ac345d75e15156bf7795c99c0d7d68aa5a
GaziRiad <ouhibimustapha36@gmail.com>
2023-11-19 23:05:04 +0100

    Fixing signup for laptops

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -7,14 +7,14 @@ import { navigation } from "../constants/navigation";
 function Signup() {
   return (
     <section className="pt-14 pb-14 gradient min-h-screen">
-      <header className="container mx-auto flex items-center justify-between mb-24 px-8 lg:px-0">
+      <header className="container mx-auto flex items-center justify-between mb-24 px-8 lg:px-0 lg:mb-12">
         <Logo />
         <Navigation content={navigation} />
       </header>
       <div className="container mx-auto flex justify-center items-center">
-        <div className="relative flex  justify-between bg-white px-8 py-8 rounded-xl shadow-2xl w-[95%] lg:py-14 lg:px-16 lg:w-[75%] xl:w-[60%]">
+        <div className="relative flex  justify-between bg-white px-8 py-8 rounded-xl shadow-2xl w-[95%] lg:py-12 lg:px-16 lg:w-[75%] xl:w-[60%]">
           <div className="w-full lg:w-[65%] xl:w-[55%]">
-            <p className="text-left text-primary text-4xl font-semibold mb-8">
+            <p className="text-left text-primary text-3xl font-semibold mb-8 2xl:text-4xl">
               Sign Up
             </p>
             <SignupForm />
